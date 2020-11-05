4791ffb524a180ee337fa5c01ec8998dac1c25de
TwilightVanish <jayson.koremans@gmail.com>
2020-11-05 14:44:34 +0100

    Added InternalServerException

---
+++ src/main/java/fontysin/project/Exceptions/InternalServerException.java
@@ -0,0 +1,11 @@
+package fontysin.project.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+public class InternalServerException extends RuntimeException {
+    public InternalServerException(String ex) {
+        super(ex);
+    }
+}
