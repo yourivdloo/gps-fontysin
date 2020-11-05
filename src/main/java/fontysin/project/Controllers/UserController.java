a86e46c751144ba9dd6b1b783a070681bb4751f9
TwilightVanish <jayson.koremans@gmail.com>
2020-11-05 14:51:19 +0100

    UserController improvements

--- src/main/java/fontysin/project/Controllers/UserController.java
+++ src/main/java/fontysin/project/Controllers/UserController.java
@@ -1,81 +1,69 @@
 package fontysin.project.Controllers;
 
 import fontysin.project.Exceptions.BadRequestException;
+import fontysin.project.Exceptions.InternalServerException;
 import fontysin.project.Exceptions.NotFoundException;
 import fontysin.project.Models.user.AppUser;
 import fontysin.project.Repositories.UserRepository;
 import fontysin.project.Services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @Controller
 @RequestMapping("/api/users")
 public class UserController {
-    @Autowired
-    private UserRepository userRepository;
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping(path="/all")
     public @ResponseBody ResponseEntity<Iterable<AppUser>> getAllUsers(){
         Iterable<AppUser> users = userService.getAllUsers();
-
-        if(users == null){ throw new NotFoundException("There are currently no users in the database"); }
-
-        return new ResponseEntity<Iterable<AppUser>>(users, HttpStatus.FOUND);
+        if(users == null) {
+            throw new NotFoundException("There are currently no users in the database");
+        }
+        return new ResponseEntity<>(users, HttpStatus.FOUND);
     }
 
     @GetMapping(path="/{pcn}")
     public @ResponseBody ResponseEntity<AppUser> getUserByPcn(@PathVariable int pcn){
         AppUser user = userService.getUserByPcn(pcn);
-
         if (user != null){
-            return new ResponseEntity<AppUser>(user, HttpStatus.FOUND);
+            return new ResponseEntity<>(user, HttpStatus.FOUND);
         } else {
-            throw new NotFoundException("No user found for this PCN. Please provide a valid PCN.");
+            throw new NotFoundException("A user with that PCN doesn't exist");
         }
     }
 
-//    @GetMapping(path="/id/{Id}")
-//    public @ResponseBody ResponseEntity<AppUser> getUserById(@PathVariable int Id){
-//        Optional<AppUser> user = userRepository.findById(Id);
-//        if(user.isPresent()) {
-//            return new ResponseEntity<AppUser>(user.get(), HttpStatus.FOUND);
-//        } else {
-//            throw new NotFoundException("No user found for this Id. Please provide a valid Id.");
-//        }
-//    }
-
     @PostMapping(path="/new")
-    public @ResponseBody ResponseEntity<AppUser> createUser(@RequestParam int pcn, @RequestParam(required= false) String firstName, @RequestParam(required= false) String lastName) {
-        if (Util.EmptyOrNull(new String[]{firstName, lastName})) {
-            throw new BadRequestException("The user can not be added because it is not complete");
+    public @ResponseBody ResponseEntity<AppUser> createUser(@RequestBody AppUser user) {
+        if (Util.EmptyOrNull(new String[]{user.getFirstName(), user.getLastName()})) {
+            throw new BadRequestException("The user was not created - Missing Arguments");
         }
 
-        AppUser user = new AppUser(pcn, firstName, lastName);
-        AppUser result = userService.createUser(user);
-
-        if(user!=null) {
-            return new ResponseEntity<>(result, HttpStatus.CREATED);
-        } else{
-            throw new BadRequestException("The user can not be added because it is not complete");
+        AppUser result = userService.createUser(new AppUser(Util.GetPcn(), user.getFirstName(), user.getLastName()));
+        if(result == null) {
+            throw new InternalServerException("We couldn't create the user");
         }
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 
     @DeleteMapping(path = "/{pcn}")
-    public @ResponseBody ResponseEntity deleteUser(@PathVariable int pcn){
-        boolean success = userService.deleteUser(pcn);
-
-        if(success){
-            return new ResponseEntity("User has successfully been deleted.", HttpStatus.OK);
-        } else {
-            throw new NotFoundException("User doesn't exist. Might have already been deleted.");
+    public @ResponseBody ResponseEntity<String> deleteUser(@PathVariable int pcn){
+        AppUser user = userService.getUserByPcn(pcn);
+        if(user == null) {
+            throw new NotFoundException("A user with that PCN doesn't exist");
         }
+
+        boolean success = userService.deleteUser(pcn);
+        if(!success) {
+            throw new InternalServerException("We couldn't delete the user");
+        }
+        return new ResponseEntity<>("The user with PCN " + pcn + " has been deleted.", HttpStatus.OK);
     }
 }
