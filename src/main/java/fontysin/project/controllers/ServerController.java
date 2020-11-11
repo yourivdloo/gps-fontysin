3ce4611cc78ac500e98acef536dc570627c450d0
TwilightVanish <jayson.koremans@gmail.com>
2020-11-11 13:58:59 +0100

    Replaced ServerController to specifically use GET requests

--- src/main/java/fontysin/project/controllers/ServerController.java
+++ src/main/java/fontysin/project/controllers/ServerController.java
@@ -3,12 +3,12 @@ package fontysin.project.controllers;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ServerController {
 
-    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" })
+    @GetMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" })
     public String getIndex(HttpServletRequest request) {
         return "/index.html";
     }
