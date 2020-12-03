3473cdf7fdbd6d26a5f9ad6964c4157814320e30
TwilightVanish <jayson.koremans@gmail.com>
2020-12-03 16:04:43 +0100

    Remove lingering CrossOrigin Annotation

--- src/main/java/fontysin/project/controllers/UserController.java
+++ src/main/java/fontysin/project/controllers/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin
 @Controller
 @RequestMapping("/api/user")
 public class UserController {
