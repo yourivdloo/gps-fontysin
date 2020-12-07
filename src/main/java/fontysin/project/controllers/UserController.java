6151fd57695effa7af48957043ddebfc9b162c74
Jack Mutsers <jmutsers95@hotmail.com>
2020-12-07 10:24:57 +0100

    added cross origin mapping for user controller

--- src/main/java/fontysin/project/controllers/UserController.java
+++ src/main/java/fontysin/project/controllers/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @Controller
 @RequestMapping("/api/user")
 public class UserController {
