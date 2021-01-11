413405b4363a87260715df6a05b74b9613cd4504
Nynke van Schuppen <I410078@fhict.nl>
2021-01-11 11:59:32 +0100

    tutorial-springguide-socket

---
+++ src/main/java/fontysin/project/controllers/ChatController.java
@@ -0,0 +1,18 @@
+package fontysin.project.controllers;
+
+import fontysin.project.websocket.Greeting;
+import fontysin.project.websocket.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
---
+++ src/main/java/fontysin/project/websocket/Greeting.java
@@ -0,0 +1,17 @@
+package fontysin.project.websocket;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
