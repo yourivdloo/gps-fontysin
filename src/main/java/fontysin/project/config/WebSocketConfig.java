a7a55dfdb65bb50a27f5e60d5d25ac254ef0ccfc
TwilightVanish <jayson.koremans@gmail.com>
2021-01-12 12:24:45 +0100

    Fixed 403 on accessing websockets

--- src/main/java/fontysin/project/config/WebSocketConfig.java
+++ src/main/java/fontysin/project/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/websocket").withSockJS();
+        registry.addEndpoint("/api/websocket").setAllowedOrigins("").withSockJS();
     }
 
 }
