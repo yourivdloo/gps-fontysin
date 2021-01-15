d278ede52e973c6813402d71beaad054ee0ceced
Jack Mutsers <jmutsers95@hotmail.com>
2021-01-15 14:20:19 +0100

    removed cross origin header

--- src/main/java/fontysin/project/controllers/ChatController.java
+++ src/main/java/fontysin/project/controllers/ChatController.java
@@ -7,9 +7,7 @@ import fontysin.project.services.UserService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
 @Controller
 public class ChatController {
 
